252d03158fd8a564b2e5f095152e344d97e9c9fc
ashish <ashishmane245@gmail.com>
2024-03-30 19:49:42 +0530

    'Portfolio'

---
+++ script.js
@@ -0,0 +1,12 @@
+$('#second').click(function () {
+    $(document).scrollTop(100) // any value you need
+});
+
+$('#thirdsection').click(function () {
+    $(document).scrollTop(100) // any value you need
+});
+
+
+$('#contact').click(function () {
+    $(document).scrollTop(100) // any value you need
+});   
